5493fd9f4f8873b463fe7739b318dfaa5c2677f7
Chukwuemeka Torti <emekatorti@gmail.com>
2024-10-28 07:04:43 -0400

    created directive and added to product list page

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,10 +1,11 @@
-import { Component, EventEmitter, Input, Output, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit, OnChanges, SimpleChanges, ChangeDetectionStrategy } from '@angular/core';
 import { Product } from '../product';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.css']
+  styleUrls: ['./product-detail.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProductDetailComponent implements OnChanges, OnInit {
   @Output() bought = new EventEmitter<string>();
@@ -22,11 +23,10 @@ export class ProductDetailComponent implements OnChanges, OnInit {
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    const product = changes['name'];
+    const product = changes['product'];
     if (!product.isFirstChange()) {
       const oldValue = product.previousValue.name;
       const newValue = product.currentValue.name;
-      console.log(`firsthand is ${product.firstChange}`);
       console.log(`Product changed from ${oldValue} to ${newValue}`);
     }
   }
